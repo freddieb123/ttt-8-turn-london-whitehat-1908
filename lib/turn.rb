cdd4519c48c1d8148376362b7b2817420862667b
fred.burgess <fred.burgess@whitehat.org.uk>
2019-08-07 16:31:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,18 @@
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def print_board(board)
+  divider = "---------"
+  print_line(board[0], board[1], board[2])
+  puts divider
+  print_line(board[3], board[4], board[5])
+end
+
+def print_line(cellOne, cellTwo, cellThree)
+  puts " #{cellOne} |  #{cellTwo} | #{cellThree} "
+end
+
 def valid_move?(board, index)
   # if index.between?(1,9)
   #   if !position_taken?(board, index)
@@ -8,6 +23,3 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def position_taken?(board, index)
-  board[index] != " "
-end
