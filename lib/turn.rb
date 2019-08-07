e9a0d77ead85c98ce2b03da0580705ca9f275930
fred.burgess <fred.burgess@whitehat.org.uk>
2019-08-07 16:31:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,6 +7,8 @@ def print_board(board)
   print_line(board[0], board[1], board[2])
   puts divider
   print_line(board[3], board[4], board[5])
+  puts divider
+  print_line(board[6], board[7], board[8])
 end
 
 def print_line(cellOne, cellTwo, cellThree)
